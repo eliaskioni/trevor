65b10730c6e2103cffa17c80734474b1a231b14a
EliasKioni <eliaskioni94@gmail.com>
2019-05-14 08:40:41 +0300

    revert to sqlite

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://dt_admin:dt2016@localhost/dreamteam_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
